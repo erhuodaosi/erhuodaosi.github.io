e36c3395c836e2311cf04542daf6efd323a039c3
fl <45588148+erhuodaosi@users.noreply.github.com>
2024-07-28 22:42:46 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,10 @@
 // 图片数组
 const images = [
-  './images/1.png',
-  './images/2.png',
-  './images/3.png',
-  './images/4.png',
-  './images/5.png',
-  './images/6.png',
-  './images/7.png',
-  './images/8.png',
-  './images/9.png'
+  './images/1.jpg',
+  './images/2.jpg',
+  './images/3.jpg',
+  './images/4.jpg',
+  './images/5.jpg'
 ]
 
 const slideshowImage = document.getElementById('slideshowImage')
